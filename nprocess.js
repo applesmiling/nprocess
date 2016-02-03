24ad725d7630a41c1f0adb6a53c32278a948c9dc
anpandu <anpandumail@gmail.com>
2016-02-03 15:03:40 +0700

    run instance

--- nprocess.js
+++ nprocess.js
@@ -6,17 +6,67 @@
  * Module dependencies
  */
 
+var MProcess = require("mprocess")
+var ThrottleInstance = new (require("throttle-exec"))(10)
+var Promise = require("bluebird")
+var underscore = require("underscore")
+
 /**
  * Module exports
  */
 
-module.exports = nprocess;
+
+function callScript(processArgs){
+
+  return new Promise(function(resolve,reject){
+    var code = null
+    var output = "" 
+    var after = underscore.after(2, function(){
+      if(code == 0){
+        var n = output.length-1
+        output = (output[n]==='\n') ? output.slice(0, n) : output
+        resolve(output)
+      }else{
+        reject(new Error("Error on executing script : "+code.code))
+      }
+    })
+    var realArgs = processArgs.slice(0)
+    var cmdStr = realArgs[0]
+    var cmdArgs = realArgs.slice(1)
+    var instance = new MProcess(cmdStr, cmdArgs, MProcess.SPAWN, { cwd : __dirname })
+    instance.run()
+    instance
+      .getProcess()
+      .stdout
+      .on("data",function(out){
+        output += out.toString()
+      })
+      .on("close",function(){
+        after()
+      })
+    instance
+      .done(function(c){
+        code = c
+        after()
+      })  
+  })
+}
+
+function run (command){
+	var processArgs = command.split(' ')
+  return callScript(processArgs)
+}
+
+
+ThrottleInstance.registerFunction("run", run)
+module.exports = {
+  run: function(str) {
+    return ThrottleInstance.registerAction("run",[str])
+  },
+}
 
 /**
  * @param {}
  * @return {}
  * @api public
  */
-
-function nprocess() {
-}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,30 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect
+var Promise = require('bluebird')
+var nprocess = require('../nprocess')
 
 describe('nprocess()', function () {
-    it('does something', function () {
-        expect(true).to.equal(false);
-    });
-});
+
+  it('run single instance', function () {
+    return nprocess
+      .run('node ./resource/fixtures/fib.js 10')
+      .then(function(result){
+        expect(result).to.equal('55')
+      })
+  })
+
+  it('run multiple instance', function () {
+    var params = [ '30', '31', '32', '33', '34']
+    var answers = [ '832040', '1346269', '2178309', '3524578', '5702887' ]
+    var proms = params
+      .map(function (param) {
+        return nprocess.run('node ./resource/fixtures/fib.js '+param)
+      })
+    return Promise.all(proms)
+      .then(function (result) {
+        result.forEach(function (item, idx) {
+          expect(item).to.equal(answers[idx])
+        })
+      })
+  })
+
+})
